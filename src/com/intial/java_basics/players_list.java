e4893ced96e5b74b59e39387212b45b57b3efff2
Admin <praveen.siran16@gmail.com>
2025-01-06 07:22:51 +0530

    1-6-2025

---
+++ src/com/intial/java_basics/players_list.java
@@ -0,0 +1,10 @@
+package com.intial.java_basics;
+
+public class players_list {
+    public static void main(String[] args) {
+
+        System.out.println("Dhoni");
+        System.out.println("kohli");
+        System.out.println("bumrah");
+    }
+}
